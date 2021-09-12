9e703ff93f14a5b397036018554f1a3fb315301f
Esteban <estebanvargas10@gmail.com>
2021-09-12 17:18:04 -0600

    Event Handlers

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hi There!');
+import { User } from './models/User';
+
+const user = new User({ name: 'MyName', age: 20 });
+
+user.on('change', () => {
+  console.log('Change 01');
+});
+
+user.on('change', () => {
+  console.log('Change 02');
+});
+
+user.on('save', () => {
+  console.log('Save was triggered');
+});
+
+// console.log(user);
+
+user.trigger('save');
